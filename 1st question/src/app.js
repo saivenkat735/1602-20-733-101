def589254e85309b4a9081c5fd0c7b7d24638dab
KARNATI SAI VENKAT <90895995+saivenkat735@users.noreply.github.com>
2023-08-09 15:13:11 +0530

    Create app.js

---
+++ 1st question/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NumberList from './components/NumberList';
+
+function app() {
+  return (
+    <div className="app">
+      <h1>Number Management App</h1>
+      <NumberList />
+    </div>
+  );
+}
+
+export default app;
